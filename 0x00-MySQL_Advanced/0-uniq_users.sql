a35c0226b468b2a4bc56e0ac5251350a6f42e308
Ismael Nyambu <133232846+Isma3l-19@users.noreply.github.com>
2024-06-12 13:34:40 +0000

    In and out

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,6 +2,6 @@
 -- id, email, name
 CREATE TABLE IF NOT EXITS users (
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    email varchar(255) NOT NULL,
+    email varchar(255) NOT NULL UNIQUE,
     name varchar(255)
 )
